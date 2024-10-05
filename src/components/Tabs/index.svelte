126c4a329e7ee2560439d762bf810a984c47ba58
jjustin-35 <jjusttin11@gmail.com>
2024-10-05 22:07:11 +0800

    update: tabs css

--- src/components/Tabs/index.svelte
+++ src/components/Tabs/index.svelte
@@ -5,7 +5,7 @@
   export let onTabChange: (tab: string) => void;
 </script>
 
-<Tabs.Root value={activeTab} class="border-b px-4 pb-4 min-w-full overflow-x-scroll">
+<Tabs.Root value={activeTab} class="border-b px-4 pb-4 min-w-full overflow-x-scroll no-scrollbar touch-pan-x">
   <Tabs.List class="flex justify-around bg-yellow-400 p-0">
     {#each tabs as tab}
       <Tabs.Trigger value={tab} class="text-center text-white" on:click={() => onTabChange(tab)}>
